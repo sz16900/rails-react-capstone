bdbd99da06cc875cb59ef3f6f6b0e49b303469a2
sz16900 <sz16900@my.bristol.ac.uk>
2020-10-01 12:22:45 -1000

    modalReview cleanup

--- app/javascript/components/Coach/ModalReview.js
+++ app/javascript/components/Coach/ModalReview.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import React from 'react';
 import ReviewForm from './ReviewForm';
 
@@ -16,18 +14,13 @@ export default function Modal() {
         Review!
       </button>
       {showModal ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            onClick={() => setShowModal(false)}
-          >
+        <div>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <ReviewForm />
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black" />
-        </>
+        </div>
       ) : null}
     </>
   );
 }
-
-/* eslint-enable */
